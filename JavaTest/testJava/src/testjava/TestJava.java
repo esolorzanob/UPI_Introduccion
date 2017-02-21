7655a1ee5ef5c55a2e07a4149c0ea67a36caf5df
esolorzanob <esolorzano@renovatiocloud.com>
2017-02-21 08:18:27 -0600

    Subido Proyecto

---
+++ JavaTest/testJava/src/testjava/TestJava.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testjava;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Esteban
+ */
+public class TestJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        System.out.println("Digite el número");
+        Scanner keyboard = new Scanner(System.in);
+        String test = keyboard.nextLine();
+        int test2 = keyboard.nextInt();
+                
+        ArrayList<Integer> numeros = new ArrayList<>();
+        
+        int capacidad = 10;
+        String[] nombres = new String[capacidad];
+        for(int i =0; i < capacidad; i++){
+            nombres[i] = "Hola" + i;
+        }
+        System.out.println(Arrays.toString(nombres));
+         */
+        /*
+        Scanner keyboard = new Scanner(System.in);
+        int opcion = 1;
+        while (opcion != 2) {
+            System.out.println("Digite la edad");
+            int edad = keyboard.nextInt();
+            if (edad < 0) {
+                System.out.println("No ha nacido");
+            } else if (edad >= 0 && edad <= 11) {
+                System.out.println("Es un niño");
+            } else if (edad > 11 && edad <= 17) {
+                System.out.println("Es un adolescente");
+            } else if (edad > 17 && edad <= 60) {
+                System.out.println("Es un adulto");
+            } else if (edad > 60 && edad <= 120) {
+                System.out.println("Es un adulto mayor");
+            } else {
+                System.out.println("Es un vampiro");
+            }
+            System.out.println("Digite 1 si desea continuar o 2 si desea salir");
+            opcion = keyboard.nextInt();
+        }
+         */
+        int test = 20;
+        System.out.println(test);
+        
+        System.exit(0);
+
+    }
+}
---
+++ JavaTest/testJava/src/testjava/sumar.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testjava;
+
+/**
+ *
+ * @author Esteban
+ */
+public class sumar {
+    private int x = 0;
+     public sumar() {
+        
+    }
+     
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public int sumarNum(){        
+    return getX() + 10;
+    }
+   
+   
+     
+}
